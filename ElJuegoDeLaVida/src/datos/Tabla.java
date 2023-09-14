4a7e86a34bc89d00dcdddb007efa030e10e86293
Waldosir <kachiku98@hotmail.com>
2023-09-13 17:51:26 -0700

    Programa principal del juego de la vida casi terminado.

---
+++ ElJuegoDeLaVida/src/datos/Tabla.java
@@ -0,0 +1,147 @@
+package datos;
+
+public class Tabla {
+	//Valor de fila, columna y datos de tabla
+	int fila,columna;
+	boolean[][] datos;
+	
+	public Tabla(int fila, int columna, boolean datos[][]) { //Constructor para introducir datos
+		this.fila = fila;
+		this.columna = columna;
+		this.datos = datos;
+	}
+	
+	public Tabla(int fila, int columna) {//Constructor sin tabla de datos
+		this.fila = fila;
+		this.columna = columna;
+	}
+	
+	//Ingresar nueva tabla
+	public void setDatos(boolean[][] datos) {
+		this.datos = datos;
+	}
+	
+	//Obtener datos de la tabla
+	public boolean[][] getDatos() {
+		return datos;
+	}
+	
+	//Tabla generada con los datos de la tabla
+	public String toString(){
+		int filas = this.fila;
+		int columnas = this.columna;
+		
+		String FormaTabla = "   ";//Espacio entre los 0 del inicio de la tabla
+		for(int i=0;i<columnas;i++) {//Hacer la primera fila
+			if(i<10) {
+				FormaTabla +=i+"  ";//Caso unidades
+			}else {
+				FormaTabla +=i+" ";//Caso decenas
+			}
+			
+		}
+		FormaTabla +="\n";//Salto de linea
+		
+		for(int i=0;i<filas;i++) { //For de filas
+			if(i<10) {//La columna inicial
+				FormaTabla  += i+"  ";//Unidades
+			}else {
+				FormaTabla +=i+" ";//Decenas
+			}
+			
+			for(int j=0;j<columnas;j++)//For de columnas
+			{
+				if(this.datos[i][j]) { //Caso donde organismos esten vivos
+					FormaTabla +="X  ";
+				}
+				else {//Caso donde organismos esten muertos
+					FormaTabla+="-  ";
+				}
+			}
+			FormaTabla+="\n";//Salto de linea
+		}
+		
+		return FormaTabla;
+		
+	}
+	
+	public boolean[][] TablaNueva() {//Algoritmo para verificar organismos vivos o muertas
+		boolean[][] Newdatos  = new boolean[datos.length][datos[2].length];//Colocar tabla de datos
+		
+		for(int i =0;i<this.datos.length;i++) {//For Fila
+			for(int j=0;j<this.datos[2].length;j++) {//For Columna
+				Newdatos[i][j] = VerificaSiVive(i,j);//Hace el acomodo por condiciones		
+			}
+		}
+		return Newdatos;//Regresa la nueva tabla de datos
+	}
+	
+	private boolean VerificaSiVive(int PosFila, int PosColumna) {
+		int NumVivos = 0;
+		boolean dato = datos[PosFila][PosColumna];
+		for(int i = PosFila-1;i<=PosFila+1;i++) {
+			for(int j = PosColumna-1;j<=PosColumna+1;j++) {
+				/*Condiciones del if. Se deben de cumplir todas las condiciones:
+				 * 1)Comparan valores del arreglo que sean mayores de 0
+				 *2)Comparan valores del arreglo que no esten fuera del rango de la tabla
+				 *3)El valor actual no puede entrar en la comparacion, dado que no es un vecino
+				 */
+				if((i>=0 && j>=0) &&(i<this.datos.length && j<this.datos[2].length)
+						&&!(i==PosFila && j ==PosColumna) ) {
+					if(this.datos[i][j]) {//Si encuentra un vecino vivo
+						NumVivos++;
+					}
+				}
+			}
+		}
+		
+		/*if para caso que este vivo. Se deben de cumplir las 3 condiciones:
+		 * 1)Si esta vivo
+		 * 2)El numero de vecinos vivos es mayor o igual a 3
+		 * 3)El numero de vivos es menor o igual a 3 
+		 */
+		if(dato && NumVivos>=2 && NumVivos<=3) {
+			return true;
+		}else if(!dato && NumVivos == 3) {//si no esta vivo y los vecinos vivos son exactamente 3
+			return true;
+		}
+		//Si no se cumplen los casos anteriores
+		return false;
+	
+	}
+	
+	//Verifica si las tablas son iguales
+	private boolean TablasIguales(boolean datosT2[][]) {
+		boolean SonIguales = true;//Condicion de que si es la misma tabla
+		
+		for(int i = 0; i<datos.length;i++) {
+			for(int j = 0; j<datos[2].length;j++) {
+				if(this.datos[i][j]!=datosT2[i][j]) {//Si el valor llegara a ser distinto
+					SonIguales = false;//Es falso
+				}
+			}
+		}
+		
+		return SonIguales;
+	}
+	
+	//Detiene el programa bajo las condiciones adecuadas
+	public boolean DetenerPrograma(boolean datosT2[][]) {
+		if(TablasIguales(datosT2)) {//Tablas iguales
+			System.out.print("La generacion anterior es igual a la actual.");
+			return true;
+		}
+		
+		//Verifica si hay un organismo vivo por toda la tabla
+		for(int i =0;i<this.datos.length;i++) {
+			for(int j=0;j<this.datos[2].length;j++) {
+				if(this.datos[i][j]) {//Encuentra un solo ser vivo
+					return false;//No se detiene el programa
+				}
+			}
+		}
+		//Sale del ciclo, y no encontro un ser vivo
+		System.out.print("Ya no hay seres vivos en la generacion actual.");
+		return true;
+	}
+}
---
+++ ElJuegoDeLaVida/src/principal/MainPrincipal.java
@@ -0,0 +1,116 @@
+package principal;
+
+import java.util.Scanner;
+
+import datos.Tabla;
+
+public class MainPrincipal {
+	
+
+	static int MinimoFilasTablero = 2, MaximoFilasTablero = 20; //Rango fila tablero
+	static int MinimoColumnasTablero = 2, MaximoColumnasTablero = 20; //Rango columna tablero
+	static int MinimoGeneraciones = 1, MaximoGeneraciones = 50; //Rango generaciones
+
+	
+	//Ingreso de datos del usuario en su respectivo rango con su pregunta
+	static int IngresoEnteroRango(String TextoPregunta, int minimo, int maximo) {
+				Scanner sc = new Scanner(System.in);
+				int numero = 0;
+				while(true) {
+					System.out.print(TextoPregunta); //Hace la peticion del dato
+					try{
+						numero = Integer.parseInt(sc.nextLine());
+						if(numero>=minimo && numero<=maximo) { //Verifica que este en el rango
+							break;
+						}else {// Si no esta en el rango
+							System.out.print("Valor fuera del rango. Ingrese un valor dentro del rango\n");
+						}
+						
+					}
+					catch(Exception e) {
+						System.out.println("Error. Valor no valido. Ingrese el valor nuevamente");
+						
+					}
+					
+					
+				}
+				return numero; //Regresa el numero y sale del ciclo
+				
+		}
+
+	
+	
+	
+	static void Pausar() {
+		 System.out.println("Press Enter key to continue...");
+	        try
+	        {
+	        	Scanner sc = new Scanner(System.in);
+	        	sc.nextLine();
+	            
+	        }  
+	        catch(Exception e)
+	        {}  
+	}
+
+
+	public static void main(String[] args) {
+		
+		
+		//Ingreso de fila, columna y generaciones por parte del usuario
+		int NumFilas = IngresoEnteroRango("Ingrese numero de filas del tablero: ",
+				MinimoFilasTablero, MaximoFilasTablero);
+		
+		int NumColumnas = IngresoEnteroRango("Ingrese numero de columnas del tablero: ",
+				MinimoColumnasTablero, MaximoColumnasTablero);
+		int NumGeneraciones = IngresoEnteroRango("Ingrese el numero de generaciones del juego: ",
+				MinimoGeneraciones,MaximoGeneraciones);
+		
+		//Minimo y maximo del organismo
+		int MinimoOrganismos = 0,MaximoOrganismos = (int) (NumFilas*NumColumnas*.5);
+		//Valores Minimo y Maximo del arreglo para introducir datos
+		int MinimoArreglo = 0, MaximoArregloF = NumFilas-1, MaximoArregloC = NumColumnas-1;
+		//Ingreso de numero de organismos
+		int NumeroOrganismos = IngresoEnteroRango("Ingrese numero de organismos iniciales: ",
+				MinimoOrganismos,MaximoOrganismos);
+		
+		//Tabla de ingreso de datos por parte del usuario
+		boolean[][] datos  = new boolean[NumFilas][NumColumnas];
+		for(int i=0; i<NumeroOrganismos;i++) {//Bucle para ingresar los datos
+			while(true) {//Bucle para evitar que se introduzcan datos en una posicion que ya este viva la casilla
+				int f = IngresoEnteroRango("Fila del organismo "+(i+1)+": ",MinimoArreglo,MaximoArregloF);
+				int c = IngresoEnteroRango("Columna del organismo "+(i+1)+": ",MinimoArreglo,MaximoArregloC);
+				System.out.println();
+				if(datos[f][c]!=true) {//Si en esa casilla no hay ser vivo
+					datos[f][c] = true; //Los datos de esa fila-columna se vuelven true
+					break;//Sale del While hacia el siguiente caso
+				}else {//Caso donde ya fue introducido en esa posicion -Entra en bucle
+					System.out.println("Ya hay un ser vivo en esta coordenada."
+							+ "Introducir en otra coordenada");
+				}
+				
+			}
+			
+		}
+		
+		//Tablas: Una actual del programa, y otra para comparar
+		Tabla TVieja  = new Tabla(NumFilas,NumColumnas);
+		Tabla TNueva = new Tabla(NumFilas, NumColumnas,datos);
+		
+		for(int i=0; i<NumGeneraciones;i++) {
+			System.out.println("Generacion "+(i+1));
+			System.out.println(TNueva.toString()); //Tabla con los datos
+			
+			//Si la tabla vieja tiene datos, y si la condicion de detener la tabla se hace
+				if(TVieja.getDatos()!=null && TNueva.DetenerPrograma(TVieja.getDatos())) {
+					break;//Termina el ciclo de generaciones
+				}
+			TVieja.setDatos(TNueva.getDatos()); //Coloca los datos de la tabla actual para ser datos antiguos
+			TNueva.setDatos(TVieja.TablaNueva());//Actualiza los datos de la tabla actual con los datos antiguos
+			Pausar();      
+		}
+		
+		System.out.print("Fin del programa.");
+
+	}
+}
